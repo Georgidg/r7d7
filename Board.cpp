48ae65d910e5fc705e2896f29ffd582a24131074
GeorgiDg <33101215+Georgidg@users.noreply.github.com>
2018-05-23 23:10:39 +0300

    Update Board.cpp
    _

--- Board.cpp
+++ Board.cpp
@@ -1,15 +1,15 @@
 
 #include "Board.h"
-Board::Board(int size) {
-	this->size =size;
-	this->buildBoard = new BuildBoard*[size];
-	for (int i=0;i<size;i++) {
-		buildBoard[i] = new BuildBoard[size];
+Board::Board(int _size) {
+	this->_size =_size;
+	this->buildBoard = new BuildBoard*[_size];
+	for (int i=0;i<_size;i++) {
+		buildBoard[i] = new BuildBoard[_size];
 	}
 }
 ostream & operator<<(ostream & os,const Board & board) {
-	for(int i = 0;i<board.size;i++){
-		for(int j = 0;j<board.size;j++){
+	for(int i = 0;i<board._size;i++){
+		for(int j = 0;j<board._size;j++){
 			os<< board.buildBoard[i][j];
 		}
 		os<<"\n";
@@ -19,8 +19,8 @@ ostream & operator<<(ostream & os,const Board & board) {
 
 Board& Board::operator=(char c) {
 	if(c=='X'||c=='O'||c=='.') {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
+		for (int i = 0; i < _size; i++) {
+			for (int j = 0; j < _size; j++) {
 				this->buildBoard[i][j] = c;
 			}
 		}
@@ -30,20 +30,20 @@ Board& Board::operator=(char c) {
 }
 Board& Board::operator=(Board & board) {
 	this->~Board();
-	this->size = board.size;
-	this->buildBoard = new BuildBoard*[board.size];
-	for (int i=0;i<board.size;i++) {
-		buildBoard[i] = new BuildBoard[board.size];
+	this->_size = board._size;
+	this->buildBoard = new BuildBoard*[board._size];
+	for (int i=0;i<board._size;i++) {
+		buildBoard[i] = new BuildBoard[board._size];
 	}
-	for (int i = 0; i < board.size; i++) {
-		for (int j = 0; j < board.size; j++) {
+	for (int i = 0; i < board._size; i++) {
+		for (int j = 0; j < board.s_ize; j++) {
 			this->buildBoard[i][j].set(board.buildBoard[i][j].get());
 		}
 	}
 	return *this;
 }
 BuildBoard& Board::operator[](vector<int> vec){
-	if(vec[0]>=0 && vec[1]>=0 && vec[0]<size && vec[1]<size){
+	if(vec[0]>=0 && vec[1]>=0 && vec[0]<_size && vec[1]<_size){
 		return this->buildBoard[vec[0]][vec[1]];
 	}
 	else throw IllegalCoordinateException(vec[0],vec[1]);
@@ -52,26 +52,26 @@ BuildBoard& Board::operator[](vector<int> vec){
 
 Board::Board()
 {
-	this->size=0;
+	this->_size=0;
 	this->buildBoard =NULL;
 }
 
 Board::Board(const Board& board)
 {
-	this->size= board.size;
-	this->buildBoard = new BuildBoard*[size];
-	for (int i=0;i<size;i++) {
-		buildBoard[i] = new BuildBoard[size];
+	this->_size= board._size;
+	this->buildBoard = new BuildBoard*[_size];
+	for (int i=0;i<_size;i++) {
+		buildBoard[i] = new BuildBoard[_size];
 	}
-	for (int i = 0; i < board.size; i++) {
-		for (int j = 0; j < board.size; j++) {
+	for (int i = 0; i < board._size; i++) {
+		for (int j = 0; j < board._size; j++) {
 			this->buildBoard[i][j].set(board.buildBoard[i][j].get());
 		}
 	}
 }
 
 Board::~Board(){
-    for (int i = 0; i <size ; ++i) {
+    for (int i = 0; i <_size ; ++i) {
             delete[] buildBoard[i];
     }delete[] buildBoard;
 }
